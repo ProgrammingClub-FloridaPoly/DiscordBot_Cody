249b728f66a69f26ca36478cc7c45a1eac4ea3f8
Jose Placeres <jplaceres3559@floridapoly.edu>
2020-07-20 23:45:27 -0400

    fixed user input in code block command

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const config = {
     token: process.env.TOKEN,
     owner: process.env.OWNER,
     prefix: process.env.PREFIX,
+    codeblock: process.env.CODEBLOCK,
     generalChat: process.env.GENERALCHAT,
     testingChat: process.env.TESTINGCHAT,
 };
@@ -18,51 +19,45 @@ const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith(
 
 for(const file of commandFiles){
     const command = require(`./commands/${file}`);
-
     client.commands.set(command.name, command);
 }
 
-//////////////////////////////////////////////////////////////////
-
-//listens to see if bot is online (in CMD type: node .)
+//listens to see if bot is online (in CMD, type: node .)
 client.once('ready', () => {
-    
     console.log('Cody is online!');
 
     // Accounces that to specific chat that it is online.
-    
     // var channelID = config.generalChat;
     var channelID = config.testingChat;
     var hello = 'TESTING MODE!';
     client.channels.cache.get(channelID).send(hello);
-
 });
 
 //makes bot listen to messages and commands
 client.on('message', message=>{
 
-    if(!message.content.startsWith(config.prefix) || message.author.bot) return;
+    if(!message.content.startsWith(config.codeblock) || message.author.bot) return;
 
-    const args = message.content.slice(config.prefix.length).split(/ +/);
+    const inBlock = /[`\n]+/g;     //identifies ` and ENTER
+    const existing = message.content.replace(inBlock, ' ').trim();
+
+    //only substrings out the code block quotes
+    const noBlock = message.content.substring(3, message.content.length -3);
+    console.log('1) Message.content = ' + message.content);
+    console.log('2a) noBlock = ' + noBlock);
+    console.log('2b) existing = :::' + existing + '::');
+
+    const args = existing.slice(config.prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
-
-    var mNum = message.edits;
-
-    console.log('0) ' + message);   //returns message ID
-    console.log(message.edits);
-    console.log('1) ' + args);
-    console.log('2) ' + command);
-
-    // //commands
-    // if(command === 'ding'){
-    //     message.channel.send('dong!');
-    // }
-    // else if (command == 'ping'){
-    //     client.commands.get('ping').execute(message, args);
-    // }
-    // else if (command == 'school'){
-    //     client.commands.get('school').execute(message, args);
-    // }
+    
+    //MESSAGE INFO
+//    console.log(message);   //returns message ID
+//    console.log(message.edits);     //returns attributes of message and channel
+//    console.log(typeof message);
+    console.log('3b) args = ' + args);
+    console.log('4b) command = ' + command);
+    console.log('-------------------------------------');
+    
 
     //Dynamically analyze commands
     if (!client.commands.has(command)) 
@@ -75,20 +70,10 @@ client.on('message', message=>{
         message.channel.send('```>Error: No response. Please try again.```');
     }
 
-
-    //COMMANDS
-
-
-
     //EMBED
 
 
-
-
-
-
 });
-
 
 //Keep in last line of file
 client.login(config.token);
